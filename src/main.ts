0ee26c9abbb056cadabb9ea236b082b172cf75cb
mohammadkamraan <mohammadkammraan@gmail.com>
2025-05-04 22:46:47 +0330

    feat(helmet): use helmet in project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap() {
     type: VersioningType.URI,
     defaultVersion: '1'
   })
+  app.use(helmet())
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
